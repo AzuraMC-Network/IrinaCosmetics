b89920d5e624f56259d065c36aff369de055f528
BaiJiu <Sakura_BaiJiu@outlook.com>
2024-12-09 04:17:09 +0800

    创建 PlayerData 类

---
+++ src/main/java/cn/sakura/irinacosmetics/data/PlayerData.java
@@ -0,0 +1,21 @@
+package cn.sakura.irinacosmetics.data;
+
+import java.util.UUID;
+
+public class PlayerData {
+    public PlayerData() {}
+
+    public void loadPlayerData(UUID playerUUID) {}
+
+    public void savePlayerData(UUID playerUUID) {}
+
+    public PlayerData getPlayerData(UUID playerUUID) {
+        return null;
+    }
+
+    public PlayerData getUnlockedCosmetics() {
+        return null;
+    }
+
+
+}
